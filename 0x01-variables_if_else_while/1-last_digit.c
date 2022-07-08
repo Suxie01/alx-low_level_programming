c910f38ee5fa124eea856c1a75f3a35ca25e3249
Success <successnna2020@gmail.com>
2022-07-08 04:29:43 -0700

    Greater or less than

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Last digit of");
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					printf("is");
+					if (n > 5)
+					{
+						printf("and is greater than 5\n");
+					}
+					else if (n == 0)
+					{
+						printf("and is 0\n");
+					}
+					else if (n < 6 && n != 0)
+					{
+						printf("and is less than 6 and not 0\n");
+					}
+
+					return (0);
+}
